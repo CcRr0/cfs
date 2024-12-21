14cdd4df25a80212af0dbffea875c4cd321f7e92
cr <c.r.dev@icloud.com>
2024-12-22 00:02:30 +0900

    .

--- src/content_scripts/contest/submit/main.ts
+++ src/content_scripts/contest/submit/main.ts
@@ -1,14 +1,14 @@
 const id: string | null = (new URLSearchParams(location.search)).get("id");
 
-if (id) {
-    if (document.readyState !== "loading") {
-        void main();
-    } else {
-        document.addEventListener("DOMContentLoaded", main);
-    }
+if (document.readyState !== "loading") {
+    void main();
+} else {
+    document.addEventListener("DOMContentLoaded", main);
 }
 
 async function main() {
-    const select: HTMLSelectElement = document.querySelector("select[name='submittedProblemIndex']")!;
-    select.value = id!;
+    if (id) {
+        const select: HTMLSelectElement = document.querySelector("select[name='submittedProblemIndex']")!;
+        select.value = id;
+    }
 }
